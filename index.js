3c8b8d5c56c762deac8dc48b2943584ee6df6aa8
Shivam Maurya <146646856+ShivamMauryaCoding@users.noreply.github.com>
2025-08-14 22:18:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config;
 
 app.use(
   cors({
-    origin: "https://notes-app-fe-bice.vercel.app/",
+    origin: "https://notes-app-fe-bice.vercel.app",
     credentials: true,
   })
 );
